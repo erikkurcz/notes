2652fd1f93fb05125537f8c4abfdfbedc49805e3
Live session user <ejkurcz@gmail.com>
2018-09-24 15:07:08 +0000

    adding sublime install

--- day0/download_spark_dependencies.sh
+++ day0/download_spark_dependencies.sh
@@ -23,3 +23,16 @@ sudo apt install oracle-java8-set-default
 $SPARK_HOME/bin/spark-shell
 
 # there ya go
+
+# sublime
+# http://tipsonubuntu.com/2017/05/30/install-sublime-text-3-ubuntu-16-04-official-way/
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+# can be opened off command line with 'subl'
+
+
